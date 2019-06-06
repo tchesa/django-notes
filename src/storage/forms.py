2280e2837b76efbf8cf7a691ec542b099e02ffa2
cesar <cesar@usemobile.xyz>
2019-06-06 18:53:06 -0300

    css improvements

--- src/storage/forms.py
+++ src/storage/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Note
 
 class NoteModelForm (forms.ModelForm):
-  title = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Título'}))
+  title = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Título', 'maxLength': 120}))
   content = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Conteúdo'}), label='')
   class Meta:
     model = Note
