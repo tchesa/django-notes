f4a030ba12b76d7d8bc240c7d103103212ae3e38
cesar <cesar@usemobile.xyz>
2019-06-07 11:00:03 -0300

    book header

--- src/storage/forms.py
+++ src/storage/forms.py
@@ -2,8 +2,8 @@ from django import forms
 from .models import Note
 
 class NoteModelForm (forms.ModelForm):
-  title = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Título', 'maxLength': 120}))
-  content = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Conteúdo'}), label='')
+  title = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Title', 'maxLength': 120}))
+  content = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Content'}), label='')
   class Meta:
     model = Note
     fields = ['title', 'content']
