cbf554537795be22614baf96532419626afeeef0
cesar <cesar@usemobile.xyz>
2019-06-24 13:31:32 -0300

    handling posts by user

--- src/storage/models.py
+++ src/storage/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
 
 class Note(models.Model):
+  user = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
   title = models.CharField(max_length=120)
   content = models.TextField(null=True, blank=True)
--- src/storage/views.py
+++ src/storage/views.py
@@ -1,27 +1,34 @@
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
 from .models import Note
 from .forms import NoteModelForm
 
+@login_required
 def list_note_page(request):
-  notes = Note.objects.all()
+  notes = Note.objects.filter(user=request.user)
   context = {'notes': notes}
   return render(request, 'notes.html', context)
 
+@login_required
 def view_note_page(request, id):
   note = Note.objects.get(id=id)
   context = {'note': note}
   return render(request, 'view.html', context)
 
+@login_required
 def new_note_page(request):
   form = NoteModelForm(request.POST or None) # tenta receber a requisição POST
   if form.is_valid():
-    form.save() # salva o objeto
+    obj = form.save() # salva o objeto
+    obj.user = request.user
+    obj.save()
     return redirect('/') # redireciona para a pagina inicial
   return render(request, 'new.html', {"form": form})
 
+@login_required
 def edit_note_page(request, id):
   note = Note.objects.get(id=id)
   form = NoteModelForm(request.POST or None, instance=note)
@@ -30,6 +37,7 @@ def edit_note_page(request, id):
     return redirect('/') # redireciona para a pagina inicial
   return render(request, 'edit.html', {'form': form})
 
+@login_required
 def delete_note_page(request, id):
   note = Note.objects.get(id=id)
   note.delete()
