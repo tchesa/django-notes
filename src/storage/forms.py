2d04cea1ff78b96e65d2feb123114c09332603cb
Charllon <charllondeveloper@gmail.com>
2019-06-30 22:01:26 -0300

    add: upload

--- src/storage/forms.py
+++ src/storage/forms.py
@@ -2,8 +2,8 @@ from django import forms
 from .models import Note
 
 class NoteModelForm (forms.ModelForm):
-  title = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Title', 'maxLength': 120}))
-  content = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Content'}), label='')
+  title = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Título', 'maxLength': 120}))
+  content = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Digite sua nota'}), label='')
   class Meta:
     model = Note
     fields = ['title', 'content']
